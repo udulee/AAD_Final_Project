ff1de97c8941d2faa173a6b33a76f26acdb23b3a
udulee <uduleenayanamalee@gmail.com>
2026-03-21 07:39:09 +0530

    BackEnd developed

---
+++ 02_Back_End/src/test/java/lk/ijse/_2_back_end/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse._2_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
